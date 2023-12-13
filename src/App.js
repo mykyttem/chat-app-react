7ab520f117fbd9a723bc3e3cad9053be816dfc00
chamois1 <nrabicenko5@gmail.com>
2023-12-14 01:09:32 +0200

    Added: main page

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div class="App">
+      <div class="block">
+        <div class="block-content">
+          <h2>Not allowed page</h2>
+          <h2 class="info">Pleas auth for access to content</h2>
+          <a class="info" href="/sign-up">Sign up</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
