9429c93334f8607b08963ff4fa0f44b13fbafc52
chamois1 <nrabicenko5@gmail.com>
2024-01-05 02:15:17 +0200

    Updated chat: added field input

--- src/pages/controllers/chat/Chat.js
+++ src/pages/controllers/chat/Chat.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import send from "../../../assets/icons/send.svg";
+import clip from "../../../assets/icons/clip.svg";
+import img from "../../../assets/icons/img.svg";
+
 
 const Chat = () => {
     return (
@@ -10,6 +14,30 @@ const Chat = () => {
 
             <div className="bubble-companion-message">
                 <h2 className="text-companion-message">message</h2>
+            </div>
+
+            <div className="block-input">
+                <input className="field-input" type="text" placeholder="Type something..." />
+
+                <img
+                    src={clip}
+                    className="clip"
+                    alt="clip"
+                />
+
+                <img
+                    src={img}
+                    className="img"
+                    alt="img"
+                />
+
+                <div className="box-send">
+                    <img
+                        src={send}
+                        className="send"
+                        alt="send"
+                    />
+                </div>
             </div>
         </div>
     )
